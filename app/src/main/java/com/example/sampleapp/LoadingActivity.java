213a7d3dff00c9f001ab51837e8866fc5354dc57
claudynetrixie <claudyneuy98@gmail.com>
2021-02-08 21:21:35 +0800

    addd json handler

--- app/src/main/java/com/example/sampleapp/LoadingActivity.java
+++ app/src/main/java/com/example/sampleapp/LoadingActivity.java
@@ -10,7 +10,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoadingActivity extends AppCompatActivity {
     private static final String TAG = "LoadingActivity";
@@ -34,10 +41,39 @@ public class LoadingActivity extends AppCompatActivity {
         }
 
         textAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-
         waitingPage = findViewById(R.id.loading_text);
-
         waitingPage.setAnimation(textAnim);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+//                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                    Log.d(TAG, content);
+//                    textViewResultewResult.append(content);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+//                textViewResult.setText(t.getMessage());
+                Log.d(TAG, "FAILURE");
+            }
+        });
 
     }
 }
