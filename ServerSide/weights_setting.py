fa5bff4245063d0f6fe249cbeab312a36e7c10b8
claudynetrixie <claudyneuy98@gmail.com>
2021-02-11 23:59:47 +0800

    added hobbies category

--- ServerSide/weights_setting.py
+++ ServerSide/weights_setting.py
@@ -5,6 +5,8 @@ Created on Wed Feb 10 21:51:55 2021
 @author: claudyne
 """
 
+output_gifts =10
+
 #Gender: Male, Female, Lesbian, Gay 
 #Age: Baby, Kid, Teen, Adult 
 #Occasion: Birthday, Christmas, Graduation, Just Because 
@@ -61,24 +63,95 @@ Created on Wed Feb 10 21:51:55 2021
 
 ##############
 
-# def hobbies(hobby1, hobby2, id_list):
+def get_hobbies(hobby1, hobby2, id_list, list_i):
     
+#Food: 
+#Beauty: 15816
+        
+    if(hobby1 == "Books" or hobby2 == "Books"):
+        id_list.append("13632" + "-" + list_i[1])
+        
+    if(hobby1 == "Pets" or hobby2 == "Pets"):
+        id_list.append("21292")
+        
+    if(hobby1 == "Toys" or hobby2 == "Toys"):
+        
+        if(list_i[1] == "Baby"):
+            id_list.append("115" + "-" + list_i[1])
     
+        elif(list_i[1] == "Kid"):
+            id_list.append("17309")
+            id_list.append("17328")
+            
+            if(list_i[0] == "Female"):
+                id_list.append("17320")
 
+    if(hobby1 == "Video Games" or hobby2 == "Video Games"):
+        id_list.append("20718")
+        
+    if(hobby1 == "Cooking" or hobby2 == "Cooking"):
+        id_list.append("15509")
+        
+    elif(hobby1 == "Food" or hobby2 == "Food"):
+        id_list.append("363")
+        id_list.append("21509")
+                      
+    elif(hobby1 == "Beauty" or hobby2 == "Beauty"):
+        id_list.append("15816")
+        
+    return id_list
+
+
+    
 
 # def get_unisex():
     
 
-def get_male(list):
+def assign_weights(id_list):  
+    #get len of id_list
+    #assign weight
+    weight_list = []
+    
+    length= len(id_list)
+    
+    weight= round((1/length)*output_gifts)
+    
+    for item in id_list:
+        weight_list.append(weight)
+        
+    if(sum(weight_list) != output_gifts):
+        diff = output_gifts - sum(weight_list)        
+        ctr = 0
+        
+        if diff > 0:
+            while diff != 0:
+                weight_list[ctr] += 1
+                ctr = ctr + 1
+                diff = diff - 1
+        else:
+            diff = -diff
+            while diff != 0:
+                weight_list[ctr] -= 1
+                ctr = ctr + 1
+                diff = diff - 1
+            
+        
+        
+    return weight_list
+        
+    
+    
+
+def get_male(list_i):
     id_list = []
     
-    if(list[1] =="Baby"):
+    if(list_i[1] =="Baby"):
         id_list.append("113-boy")
         
-    elif(list[1] == "Kid"):
+    elif(list_i[1] == "Kid"):
         id_list.append("113-boy")
     
-    elif (list[1] == "Teen"):
+    elif (list_i[1] == "Teen"):
         id_list.append("103-teen")
         id_list.append("2755")
         id_list.append("123")
@@ -87,20 +160,21 @@ def get_male(list):
         id_list.append("123")
         id_list.append("2755")
             
+            
     return id_list
 
 
 
-def get_female(list):
+def get_female(list_i):
     id_list = []
     
-    if(list[1] =="Baby"):
+    if(list_i[1] =="Baby"):
         id_list.append("113-girl")
         
-    elif(list[1] == "Kid"):
+    elif(list_i[1] == "Kid"):
         id_list.append("113-girl")
     
-    elif (list[1] == "Teen"):
+    elif (list_i[1] == "Teen"):
         id_list.append("102-teen")
         id_list.append("106")
         id_list.append("1713")
@@ -110,21 +184,27 @@ def get_female(list):
         id_list.append("106")
         id_list.append("1713")
         id_list.append("531")
+        
             
     return id_list
     
     
 
-def get_ids(list):
-    if(list[0] == "Male"):
-        ids = get_male(list)
-    elif(list[0] == "Female"):
-        ids = get_female(list)
+def get_ids(list_i):
+    if(list_i[0] == "Male"):
+        id_list = get_male(list_i)
+    elif(list_i[0] == "Female"):
+        id_list = get_female(list_i)
         
-    return ids
+    id_list = get_hobbies(list_i[3], list_i[4], id_list, list_i)
+    weight_list = assign_weights(id_list)
+    
+    return id_list, weight_list
         
         
 
-list= ['Female', 'Baby', 'Birthday', 'Books', 'Food']
-id_list = get_ids(list)
-print(id_list)
+# list_i= ['Male', 'Adult', 'Birthday', 'Food', 'Video Games']
+# print(list_i)
+# id_list, weight_list = get_ids(list_i)
+# print(id_list)
+# print(weight_list)
