f04503b42a2b6581492da91a9df0c1706365e4f3
claudynetrixie <claudyneuy98@gmail.com>
2021-02-12 06:20:35 +0800

    renamed card model

--- app/src/main/java/com/example/sampleapp/CardModel.java
+++ app/src/main/java/com/example/sampleapp/CardModel.java
@@ -1,10 +1,10 @@
 package com.example.sampleapp;
 
-public class Model {
+public class CardModel {
     private int image;
     private String title;
     private String desc;
-    public Model(int image, String title, String desc) {
+    public CardModel(int image, String title, String desc) {
         this.image = image;
         this.title = title;
         this.desc = desc;
