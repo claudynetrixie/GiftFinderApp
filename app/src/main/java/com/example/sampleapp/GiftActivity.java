05d4bff562396215100c43c5381f536c4861e940
claudynetrixie <claudyneuy98@gmail.com>
2021-02-12 06:28:34 +0800

    added gift activity

--- app/src/main/java/com/example/sampleapp/GiftActivity.java
+++ app/src/main/java/com/example/sampleapp/GiftActivity.java
@@ -3,6 +3,7 @@ package com.example.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+
 import android.animation.ArgbEvaluator;
 import android.os.Bundle;
 import android.widget.Adapter;
@@ -10,11 +11,14 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GiftActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     Adapter adapter;
-    List<Model> models;
+    List<CardModel> models;
     Integer[] colors = null;
     ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
@@ -23,8 +27,56 @@ public class GiftActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gift_activity);
 
-        ImageView imageView = (ImageView) findViewById(R.id.gift1);
+        //for url images
+//        ImageView imageView = (ImageView) findViewById(R.id.gift1);
+//        Glide.with(this).load("https://cf.shopee.ph/file/2a6701992b65b6c87060927a14dec831").into(imageView);
 
-        Glide.with(this).load("https://cf.shopee.ph/file/2a6701992b65b6c87060927a14dec831").into(imageView);
-    }
+        models = new ArrayList<>();
+        models.add(new CardModel(R.drawable.brochure, "Brochure", "Brochure is an informative paper document (often also used for advertising) that can be folded into a template"));
+        models.add(new CardModel(R.drawable.sticker, "Sticker", "Sticker is a type of label: a piece of printed paper, plastic, vinyl, or other material with pressure sensitive adhesive on one side"));
+        models.add(new CardModel(R.drawable.poster, "Poster", "Poster is any piece of printed paper designed to be attached to a wall or vertical surface."));
+        models.add(new CardModel(R.drawable.namecard, "Namecard", "Business cards are cards bearing business information about a company or individual."));
+        adapter = new Adapter(models, this);
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+        Integer[] colors_temp = {
+                getResources().getColor(R.color.color1),
+                getResources().getColor(R.color.color2),
+                getResources().getColor(R.color.color3),
+                getResources().getColor(R.color.color4)
+        };
+        colors = colors_temp;
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position < (adapter.getCount() -1) && position < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+                            (Integer) argbEvaluator.evaluate(
+                                    positionOffset,
+                                    colors[position],
+                                    colors[position + 1]
+                            )
+                    );
+                }
+                else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+
+
+            });
+
+
+
+
+        }
 }
